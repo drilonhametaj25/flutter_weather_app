f5dd8f61db8060b0189bf19144ac9dacd6e1fd43
drilon <drilonhametaj25@gmail.com>
2023-10-11 09:09:45 +0200

    created card

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -81,7 +81,42 @@ class WeatherScreen extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                Card(
+                  elevation: 6,
+                  child: Container(
+                    width: 100,
+                    decoration:
+                        BoxDecoration(borderRadius: BorderRadius.circular(12)),
+                    padding: const EdgeInsets.all(8.0),
+                    child: const Column(children: [
+                      Text(
+                        '03:00',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Icon(
+                        Icons.cloud,
+                        size: 32,
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        '34.127',
+                      ),
+                    ]),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
