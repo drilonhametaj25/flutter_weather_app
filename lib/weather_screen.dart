74c2827dce85887fa71e08e41c8c98d35b411269
drilon <drilonhametaj25@gmail.com>
2023-10-12 09:13:47 +0200

    extracted HourlyForecastItem

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -86,166 +86,55 @@ class WeatherScreen extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               child: Row(
                 children: [
-                  Card(
-                    elevation: 6,
-                    child: Container(
-                      width: 100,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12)),
-                      padding: const EdgeInsets.all(8.0),
-                      child: const Column(children: [
-                        Text(
-                          '03:00',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Icon(
-                          Icons.cloud,
-                          size: 32,
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Text(
-                          '34.127',
-                        ),
-                      ]),
-                    ),
-                  ),
-                  Card(
-                    elevation: 6,
-                    child: Container(
-                      width: 100,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12)),
-                      padding: const EdgeInsets.all(8.0),
-                      child: const Column(children: [
-                        Text(
-                          '03:00',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Icon(
-                          Icons.cloud,
-                          size: 32,
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Text(
-                          '34.127',
-                        ),
-                      ]),
-                    ),
-                  ),
-                  Card(
-                    elevation: 6,
-                    child: Container(
-                      width: 100,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12)),
-                      padding: const EdgeInsets.all(8.0),
-                      child: const Column(children: [
-                        Text(
-                          '03:00',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Icon(
-                          Icons.cloud,
-                          size: 32,
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Text(
-                          '34.127',
-                        ),
-                      ]),
-                    ),
-                  ),
-                  Card(
-                    elevation: 6,
-                    child: Container(
-                      width: 100,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12)),
-                      padding: const EdgeInsets.all(8.0),
-                      child: const Column(children: [
-                        Text(
-                          '03:00',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Icon(
-                          Icons.cloud,
-                          size: 32,
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Text(
-                          '34.127',
-                        ),
-                      ]),
-                    ),
-                  ),
-                  Card(
-                    elevation: 6,
-                    child: Container(
-                      width: 100,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12)),
-                      padding: const EdgeInsets.all(8.0),
-                      child: const Column(children: [
-                        Text(
-                          '03:00',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Icon(
-                          Icons.cloud,
-                          size: 32,
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Text(
-                          '34.127',
-                        ),
-                      ]),
-                    ),
-                  ),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
                 ],
               ),
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class HourlyForecastItem extends StatelessWidget {
+  const HourlyForecastItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      child: Container(
+        width: 100,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
+        padding: const EdgeInsets.all(8.0),
+        child: const Column(children: [
+          Text(
+            '03:00',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Icon(
+            Icons.cloud,
+            size: 32,
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            '34.127',
+          ),
+        ]),
       ),
     );
   }
